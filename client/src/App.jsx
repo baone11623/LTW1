8df71715304657f9402d4dcb88eed32230525c0d
Bao ne <phambaobao2k3@gmail.com>
2023-12-02 20:53:02 +0700

    setup basic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import ReactModal from "react-modal";
+import HomePage from "./page/Home/HomePage";
+import LoginPage from "./page/Auth/LoginPage";
+
+ReactModal.setAppElement("#root");
+
 function App() {
-  return <>halo</>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage></HomePage>}></Route>
+      <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { primary: ["Roboto", "sans-serif"] },
+      fontSize: {
+        heding1Black: ["30px", "46px"],
+        heading2Bold: ["26px", "40px"],
+        heading3Bold: ["18px", "28px"],
+        displayBold: ["16px", "24px"],
+        displayMedium: ["16px", "24px"],
+        bodyBold: ["14px", "22px"],
+        bodyMedium: ["13px", "18px"],
+        bodyRegular: ["12px", "18px"],
+      },
+      colors: {
+        blueColors: "#377DFF",
+        greenColors: "#38CB89",
+        yellowColors: "#FFAB00",
+        redColors: "#FF5630",
+        darkColors1: "#191C21",
+        darkColors2: "#212833",
+      },
+    },
   },
   plugins: [],
-};
+});
