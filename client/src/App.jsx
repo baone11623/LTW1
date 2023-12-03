72d46e41b5e6fe91061826b79742a73ed83ca06d
Bao ne <phambaobao2k3@gmail.com>
2023-12-03 10:55:29 +0700

    pull main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,28 @@
 import { Routes, Route } from "react-router-dom";
 import ReactModal from "react-modal";
 import LoginPage from "./page/Auth/LoginPage";
-<<<<<<< HEAD
 import LayoutCommon from "./layout/LayoutCommon";
 import ShopPage from "./page/Shop/ShopPage";
 import BlogPage from "./page/Blog/BlogPage";
 
-=======
 import RegisterPage from "./page/Auth/RegisterPage";
 import LoginSignUpLayOut from "./layout/LoginSignUpLayOut";
 import CheckMailPage from "./page/Auth/CheckMailPage";
 import ForgotPage from "./page/Auth/ForgotPage";
 import CheckMailForgotPage from "./page/Auth/CheckMailForgotPage";
 import HomePage from "./page/Home/HomePage";
->>>>>>> d270b4f09311043f2ab7219bef6b77b503d5802a
+import MyInfo from "./page/MyInfo/MyInfo";
 ReactModal.setAppElement("#root");
 
 function App() {
   return (
     <Routes>
-<<<<<<< HEAD
-      <Route path="/login" element={<LoginPage></LoginPage>}></Route>
       <Route element={<LayoutCommon></LayoutCommon>}>
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/blog" element={<BlogPage></BlogPage>}></Route>
         <Route path="/shop" element={<ShopPage></ShopPage>}></Route>
-=======
+        <Route path="/my-info" element={<MyInfo></MyInfo>}></Route>
+      </Route>
       <Route element={<LoginSignUpLayOut />}>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/register" element={<RegisterPage />}></Route>
@@ -36,11 +33,6 @@ function App() {
         path="/users/:token/:password"
         element={<CheckMailForgotPage />}
       ></Route>
-
-      <Route>
-        <Route path="/" element={<HomePage />}></Route>
->>>>>>> d270b4f09311043f2ab7219bef6b77b503d5802a
-      </Route>
     </Routes>
   );
 }
