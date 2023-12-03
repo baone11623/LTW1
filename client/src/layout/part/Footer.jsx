da5014ef541e05ce9e2e13243515ccec34f2ca5e
ASUS <ASUS@LAPTOP-SOM31BDR>
2023-12-03 10:04:27 +0700

    footer

--- client/src/layout/part/Footer.jsx
+++ client/src/layout/part/Footer.jsx
@@ -1,5 +1,42 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div>
+    <div>
+      <h2>About</h2>
+      <p></p>
+      <div>
+        <h3>Email:</h3>
+        <h3>Phone:</h3>
+      </div>
+    </div>
+    <div>
+      <h2>Quicklink</h2>
+      <div>
+        <h3>Home</h3>
+        <h3>About</h3>
+        <h3>Blog</h3>
+        <h3>Archived</h3>
+        <h3>Author</h3>
+        <h3>Contact</h3>
+      </div>
+    </div>
+    <div>
+      <h2>Category</h2>
+      <div>
+        <h3>Lifestyle</h3>
+        <h3>Technology</h3>
+        <h3>Travel</h3>
+        <h3>Business</h3>
+        <h3>Economy</h3>
+        <h3>Sport</h3>
+      </div>
+    </div>
+    <form>
+      <h2>Title</h2>
+      <p>Desc</p>
+      <input>Your Email</input>
+      <button>Subscribe</button>
+    </form>
+  </div>;
 };
 
 export default Footer;
